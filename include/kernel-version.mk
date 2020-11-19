a187521fd01935d4cdedfc6293faebc0529ebad0
John Audia <graysky@archlinux.us>
2020-11-19 10:30:16 -0500

    kernel: bump 5.4 to 5.4.80
    Removed since could be reverse-applied by quilt and found to be
    included upstream:
      backport-5.4/789-net-usb-qmi_wwan-Set-DTR-quirk-for-MR400.patch
    
    All modifications made by update_kernel.sh
    
    Build system: x86_64
    Build-tested: ipq806x/R7800, bcm27xx/bcm2711, ath79/generic
    Run-tested: ipq806x/R7800
    
    No dmesg regressions, everything functional
    
    Signed-off-by: John Audia <graysky@archlinux.us>
    Tested-by: Curtis Deptuck <curtdept@me.com> [x86_64 build/run]

--- include/kernel-version.mk
+++ include/kernel-version.mk
@@ -9,12 +9,12 @@ endif
 LINUX_VERSION-4.9 = .243
 LINUX_VERSION-4.14 = .206
 LINUX_VERSION-4.19 = .148
-LINUX_VERSION-5.4 = .77
+LINUX_VERSION-5.4 = .80
 
 LINUX_KERNEL_HASH-4.9.243 = d3aa189ca7fcc6e52d6c0333a0d7acd8789e9a492b32dbf9476e926ffaa73984
 LINUX_KERNEL_HASH-4.14.206 = 1c233efaa5063983293a02d4692acc9ced9c03e18857364855d4f612347086ac
 LINUX_KERNEL_HASH-4.19.148 = 3308c74a93ce6d4e495fde35d1351061240a895f76a288248c343713bd63555b
-LINUX_KERNEL_HASH-5.4.77 = a3e03e6970240dddc8174bf9f49b56d774c40125eabe1582d2ebe85b01addbf7
+LINUX_KERNEL_HASH-5.4.80 = 49da425c1f3c530fd3ff31d85a0461f6b6dc6e459f7faf3eee23e49a98ce64c7
 
 remove_uri_prefix=$(subst git://,,$(subst http://,,$(subst https://,,$(1))))
 sanitize_uri=$(call qstrip,$(subst @,_,$(subst :,_,$(subst .,_,$(subst -,_,$(subst /,_,$(1)))))))
